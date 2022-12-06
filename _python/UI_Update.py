eeec5cbc665303167eb29ef0a90b840aa552e061
boydstongroup <57552757+CoCl-Jerry@users.noreply.github.com>
2022-12-05 18:05:09 -0600

    update

--- _python/UI_Update.py
+++ _python/UI_Update.py
@@ -3,6 +3,6 @@ import General
 # initial GUI Update
 def init(self):
     if not General.TOF_error:
-        self.Rangefinder_data_label.setText(General.current_position + " mm")
+        self.Rangefinder_data_label.setText(str(General.current_position) + " mm")
     else:
         self.Rangefinder_data_label.setText("Error")
