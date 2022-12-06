70a900c315b835e60af9d6f4f07d1c0e1e00b934
boydstongroup <57552757+CoCl-Jerry@users.noreply.github.com>
2022-12-05 18:28:34 -0600

    Update UI_Update.py

--- _python/UI_Update.py
+++ _python/UI_Update.py
@@ -3,6 +3,6 @@ import General
 # TOF label update
 def TOF_update(self):
     if not General.TOF_error:
-        self.Rangefinder_data_label.setText(str(General.current_position) + " mm")
+        self.rangefinder_data_label.setText(str(General.current_position) + " mm")
     else:
-        self.Rangefinder_data_label.setText("Range Finder Error")
+        self.rangefinder_data_label.setText("Range Finder Error")
