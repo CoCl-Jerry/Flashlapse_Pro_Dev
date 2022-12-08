6dec4e6b20f75fcc615b0acfff63ee48d09ee177
boydstongroup <57552757+CoCl-Jerry@users.noreply.github.com>
2022-12-08 17:11:42 -0600

    Update CRC.py

--- _python/CRC.py
+++ _python/CRC.py
@@ -1,13 +1,24 @@
-def crc16(data: str, poly: hex = 0xA001) -> str:
-    """
-    CRC-16 MODBUS HASHING ALGORITHM
-    """
-    crc = 0xFFFF
+def crc16(data: str, poly: int = 0x18005) -> str:
+    # convert the hexadecimal string to a bytes object
+    data = bytes.fromhex(data)
+    # initialize the CRC to 0
+    crc = 0
+    # loop over the data, one byte at a time
     for byte in data:
-        crc ^= ord(byte)
+        # XOR the current byte with the high byte of the CRC
+        crc = crc ^ (byte << 8)
+        # initialize a mask to 0x8000 (the highest bit)
+        mask = 0x8000
+        # loop over the 8 bits of the current byte
         for _ in range(8):
-            crc = (crc >> 1) ^ poly if (crc & 0x0001) else crc >> 1
-
-    hv = hex(crc).upper()[2:]
-    blueprint = "0000"
-    return blueprint if len(hv) == 0 else blueprint[: -len(hv)] + hv
+            # if the highest bit of the CRC is set
+            if crc & mask:
+                # shift the CRC and XOR it with the polynomial
+                crc = (crc << 1) ^ poly
+            else:
+                # otherwise, just shift the CRC
+                crc = crc << 1
+            # clear the highest bit of the mask
+            mask = mask >> 1
+    # return the final CRC value as a hexadecimal string
+    return hex(crc)
