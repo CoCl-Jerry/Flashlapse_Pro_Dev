4e48f2c83c76060a33273f04b1d16a1d94c7a694
boydstongroup <57552757+CoCl-Jerry@users.noreply.github.com>
2022-12-10 13:08:34 -0600

    Update test.py

--- _python/unit tests/test.py
+++ _python/unit tests/test.py
@@ -1,8 +1,17 @@
 import time
 import serial
 
+# ser = serial.Serial(
+#     port="/dev/ttyS0",
+#     baudrate=4800,
+#     parity=serial.PARITY_NONE,
+#     stopbits=serial.STOPBITS_ONE,
+#     bytesize=serial.EIGHTBITS,
+#     timeout=1,
+# )
+
 ser = serial.Serial(
-    port="/dev/ttyS0",
+    port="/dev/ttyUSB0",
     baudrate=4800,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
