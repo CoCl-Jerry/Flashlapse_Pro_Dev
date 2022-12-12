972e85ea11882e0c10810cdeae94350f668ba198
boydstongroup <57552757+CoCl-Jerry@users.noreply.github.com>
2022-12-11 18:52:18 -0600

    update

--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@ if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
 	cd /home/pi/Documents/backup/Flashlapse_Pro_Dev
 	git pull
 fi
-sudo cp -r /home/pi/Documents/backup/Flashlapse_Pro_Dev /home/pi/Documents
+sudo cp -r /home/pi/Documents/Backup/Flashlapse_Pro_Dev /home/pi/Documents
